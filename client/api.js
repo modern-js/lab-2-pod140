8ef6fcf0f29bff7546f7999765157868ea42903b
Plamen Dobrev <plamen.orlinov@gmail.com>
2017-10-20 12:14:07 +0300

    LateSubmission

--- client/api.js
+++ client/api.js
@@ -4,13 +4,80 @@ if (typeof global === 'undefined') {
 }
 
 /* write your code here */
-
+const getEverything = new Promise(
+  function(resolve, reject) {
+    resolve(global.fetch('/food').then(
+      (x)=>{
+        //return xxx;
+        return global.fetch('/drink').then(
+          (y)=>{
+              return global.fetch('/tv').then(
+                (z)=>{
+                  return [x,y,z];
+                }
+              );
+          }
+        );
+      }
+    ));
+  }
+);
 
 
 // define the api-client object to be exported
 const api = {
-  
+
 };
+
+api.get = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.get.me = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.get.me.a = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.get.me.everything = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.go = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.go.get = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.go.get.me = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.go.get.me.a = function(something) {
+  return fetch(something, {
+    method: 'GET',
+  });
+};
+
+api.go.get.me.everything = () => getEverything;
 
 // making the code work both in the browser and node.js
 if (typeof module !== 'undefined') { module.exports = api; }
--- server/api.js
+++ server/api.js
@@ -1,5 +1,34 @@
 const api = require('express')();
 
-/* your code here */
+let flag = 0;
+api.get('/food', (req, res) => {
+    if (flag < 10) {
+        res.json({food: "Burger King"});
+        flag ++;
+    }
+    else {
+        res.json({error: "Unexpected Error"});
+    }
+});
+
+api.get('/drink', (req, res) => {
+    if (flag < 10) {
+      res.json({drink: "Coke"});
+      flag ++;
+    }
+    else {
+      res.json({error: "Unexpected Error"});
+    }
+});
+
+api.get('/tv', (req, res) => {
+    if (flag < 10) {
+        res.json({tv: "Game of Thrones"});
+        flag ++;
+    }
+    else {
+      res.json({error: "Unexpected Error"});
+    }
+});
 
 module.exports = api;
